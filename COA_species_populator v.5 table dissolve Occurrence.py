de091d5bdfdf92d15f521a7dac283c8423ca88c4
Molly Moore <mollymoore22@gmail.com>
2017-09-11 13:15:55 -0400

    updates to .Rnw file

--- COA_species_populator v.5 table dissolve Occurrence.py
+++ COA_species_populator v.5 table dissolve Occurrence.py
@@ -26,44 +26,64 @@ from operator import itemgetter
 # Set tools to overwrite existing outputs
 arcpy.env.overwriteOutput = True
 
+target_features = r'W:\Heritage\Heritage_Projects\1332_PGC_COA\COA_FileGDB\SGCN_data.gdb\PlanningUnits\PlanningUnit_Hex10acre' # planning polygon unit
+join_features = r'W:\Heritage\Heritage_Projects\1332_PGC_COA\COA_DataToServer.gdb\SGCN_Occ' # SGCN occurrence probability layer - should be polygon layer
+outGDB = r'C:\Users\mmoore\Documents\ArcGIS\output.gdb' # the output path and name of SGCN table
+scratch = r'C:\Users\mmoore\Documents\ArcGIS\output.gdb'
+counties = r'W:\LYRS\Boundaries_Political\County Hollow.lyr'
 
-target_features = arcpy.GetParameterAsText(0) # planning polygon unit
-join_features = arcpy.GetParameterAsText(1) # SGCN occurrence probability layer - should be polygon layer
-outTable = arcpy.GetParameterAsText(2) # the output path and name of SGCN table
-scratch = arcpy.GetParameterAsText(3) # scratch GDB where temporary/intermediate files are stored
+#target_features = arcpy.GetParameterAsText(0) # planning polygon unit
+#join_features = arcpy.GetParameterAsText(1) # SGCN occurrence probability layer - should be polygon layer
+#outTable = arcpy.GetParameterAsText(2) # the output path and name of SGCN table
+#scratch = arcpy.GetParameterAsText(3) # scratch GDB where temporary/intermediate files are stored
 
 ################################################################################
 # Define global variables and functions to be used throughout toolbox
 ################################################################################
 
-# Calculate intersection between Target Feature and Join Features and produces output table
-intersect = outTable
-classFeatures = ["ELCODE", "SeasonCode", "OccProb"]
-arcpy.TabulateIntersection_analysis(target_features, "unique_id", join_features, intersect, classFeatures)
+with arcpy.da.SearchCursor(counties, ['COUNTY_NAM']) as cursor:
+    county_list = sorted({row[0] for row in cursor})
+
+pu_lyr = arcpy.MakeFeatureLayer_management(target_features, "pu_lyr")
+county_lyr = arcpy.MakeFeatureLayer_management(counties, "county_lyr")
+merge_list = []
+
+for county in county_list:
+    print "working on " + county + " at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    arcpy.SelectLayerByAttribute_management(county_lyr, "NEW_SELECTION", '"COUNTY_NAM" = ' + "'%s'"%county)
+    arcpy.SelectLayerByLocation_management(pu_lyr, "INTERSECT", county_lyr, "", "NEW_SELECTION", )
+
+    # Calculate intersection between Target Feature and Join Features and produces output table
+    intersect = os.path.join(outGDB, county)
+    classFeatures = ["ELCODE", "SeasonCode", "OccProb"]
+    arcpy.TabulateIntersection_analysis(pu_lyr, "unique_id", join_features, intersect, classFeatures)
+    merge_list.append(intersect)
+
+merge = arcpy.Merge_management(merge_list, os.path.join(outGDB, "SGCNxPU_occurrence"))
 
 # delete PUs that have less than 10% (4046.86 square meters) of area overlapped by particular species
 # could change this threshold if needed
-with arcpy.da.UpdateCursor(intersect, "PERCENTAGE") as cursor:
-            for row in cursor:
-                if row[0] > 10:
-                    pass
-                else:
-                    cursor.deleteRow()
+with arcpy.da.UpdateCursor(merge, "PERCENTAGE") as cursor:
+     for row in cursor:
+        if row[0] > 10:
+            pass
+        else:
+            cursor.deleteRow()
 
 # add field to combine ELCODE and SeasonCode to have unique pairing to use in pivot table output
-arcpy.AddField_management(intersect, "El_Season", "TEXT", "", "", 50)
+arcpy.AddField_management(merge, "El_Season", "TEXT", "", "", 50)
 expression = """!ELCODE! + "_" + !SeasonCode!"""
-arcpy.CalculateField_management(intersect, "El_Season", expression, "PYTHON_9.3")
+arcpy.CalculateField_management(merge, "El_Season", expression, "PYTHON_9.3")
 
 # tabular dissolve to delete records with identical unique id, ELCODE_season, and Occurrence Probability fields
-arcpy.DeleteIdentical_management(intersect, ["unique_id", "El_Season", "OccProb"])
+arcpy.DeleteIdentical_management(merge, ["unique_id", "El_Season", "OccProb"])
 
 # groupby iterator used to keep records with highest proportion overlap
 case_fields = ["unique_id", "ELCODE", "SeasonCode"] # defining fields within which to create groups
 max_field = "PERCENTAGE" # define field to sort within groups
 sql_orderby = "ORDER BY {}, {} DESC".format(",".join(case_fields), max_field) # sql code to order by case fields and max field within unique groups
 
-with arcpy.da.UpdateCursor(intersect, "*", sql_clause=(None, sql_orderby)) as cursor:
+with arcpy.da.UpdateCursor(merge, "*", sql_clause=(None, sql_orderby)) as cursor:
     case_func = itemgetter(*(cursor.fields.index(fld) for fld in case_fields)) #get field index for field in case_fields from entire list of fields and return item
     for key, group in groupby(cursor, case_func): #grouping by case_func (unique combo of case_fields)
         next(group) #iterate through groups

1902f3e73d7af129ce81980716e368093383a068
Christopher Tracey <ctracey@paconserve.org>
2017-10-12 12:35:27 -0400

    swapped order of SGCN and habitats in knittr

--- COA_SGCN_mapmaker.R
+++ COA_SGCN_mapmaker.R
@@ -38,11 +38,15 @@ lu_sgcnSpatial1 <- lu_sgcnSpatial[lu_sgcnSpatial$El_Season %in% lu_sgcn$ELSeason
 # add in species with more than 100,000 records to skip for now.
 sp_gt100k <-as.character(c("ABPBK01010_b","ABPBJ19010_b","ABPBX45040_b","ABPBX94050_b","ABNKD06020_b","ABPBXB2020_b","ABPAE33040_b","ABPBX10030_b","ABPBX03100_b","ABPBX99010_b","ABPBX03240_b","ABPBXA0020_b","ABPBXA9010_b","ARAAD08012_y","ABPBX01020_b","ABPBX03190_b","ABPBA01010_b","ABPBX03050_b","ABNLC11010_b","ABPBX95010_b","ABPBX24010_b","ABPBX16030_b","ABPBG09050_b","ABNTA07070_b","ARADE01010_y","ABPBX11010_b","IILEPA2020_y","IILEPK4060_y","ABPAU01010_b","AMAJF05010_y","AMAFB09020_y","AFCHA05030_y","AMACC01100_b","ABNYF04040_b",
 "AMAFF08100_y","ABNME08020_b","ABNME05030_b","AMACC01130_y","ABPBJ18100_b","ABPBX01030_b","ARADE02040_y","ARAAD02040_y","ABPAU08010_b","ABPBXA0030_b","ARADE03011_y","ABPBX01060_b","ABPBX10020_b","ABPBXA4020_b","AAABH01170_y","AMABA01153_y","ABNNF19020_b"))
+#lu_sgcnSpatial1 <- lu_sgcnSpatial1[ !(lu_sgcnSpatial1 %in% sp_gt100k)]
 
-lu_sgcnSpatial1 <- lu_sgcnSpatial1[ !(lu_sgcnSpatial1 %in% sp_gt100k)]
 
+lu_sgcnSpatial1 <- lu_sgcnSpatial1[ lu_sgcnSpatial1 %in% sp_gt100k]
 
-for (i in 14:NROW(lu_sgcnSpatial1) ){   #1:NROW(lu_sgcnSpatial1)
+# only work on mammals
+lu_sgcnSpatial1 <- lu_sgcnSpatial1[which(substr(lu_sgcnSpatial1,1,2)=='AM')]
+
+for (i in 3:NROW(lu_sgcnSpatial1) ){   #1:NROW(lu_sgcnSpatial1)
   print(lu_sgcnSpatial1[i])
   # get planning unit data 
   SQLquery <- paste("SELECT unique_id, El_Season, OccProb, PERCENTAGE"," FROM lu_sgcnXpu_all ","WHERE El_Season IN (", paste(toString(sQuote(lu_sgcnSpatial1[i])), collapse = ", "), ")")
@@ -51,8 +55,7 @@ for (i in 14:NROW(lu_sgcnSpatial1) ){   #1:NROW(lu_sgcnSpatial1)
   assign(paste("sgcnXpu",lu_sgcnSpatial1[i],sep="_"), sgcnXpu ) 
   # add code to assign occ prob
   print(" species by planning units selected")
-  
-  pu_list <- get(paste("sgcnXpu_",lu_sgcnSpatial1[i],sep=""))      
+  pu_list <- get(paste("sgcnXpu_",lu_sgcnSpatial1[i],sep="")) 
   pu_list <- pu_list$unique_id
   print("planning units list generated")
   

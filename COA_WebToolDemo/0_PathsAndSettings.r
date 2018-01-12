b778825a99d4e35a3cf4cef9ebcc606d8c910988
Christopher Tracey <ctracey@paconserve.org>
2018-01-12 11:15:27 -0500

    updated rnw, specific habitat req, path script

---
+++ COA_WebToolDemo/0_PathsAndSettings.r
@@ -0,0 +1,9 @@
+# Purpose: to define a set of consistently used objects for a full COA Tool Run
+#   run. The goal is to avoid redundancy and improve consistency among scripts.
+
+# Set inputs ----
+# These locations require data created not as part of these scripts
+
+databasename <- "E:/coa2/coa_bridgetest.sqlite" 
+working_directory <- "E:/coa2/COA/COA_WebToolDemo"
+graphics_path <- "E:/coa2/COA/COA_WebToolDemo/images/"

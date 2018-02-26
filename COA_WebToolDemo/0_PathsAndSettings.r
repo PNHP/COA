77a6c22c50e0fbd595ea91fb809a8d50aff45c42
Christopher Tracey <ctracey@paconserve.org>
2018-02-26 16:46:09 -0500

    AIS, Action Revisions, etc

--- COA_WebToolDemo/0_PathsAndSettings.r
+++ COA_WebToolDemo/0_PathsAndSettings.r
@@ -10,7 +10,7 @@ graphics_path <- "E:/coa2/COA/COA_WebToolDemo/images/"
 options(useFancyQuotes = FALSE)
 
 # variables and such
-SGCN_SortOrder <- c("AB","AM","AAAB","AAAA","ARAC","ARAD","ARAA","AF","IMBIV","inv_amp","inv_bees","IIHYM","inv_beetgr","inv_beetot","IICOL","inv_buttsk","IITRI","inv_cranfl","ICMAL","IIODO","inv_flatwm","IIORT","inv_isopod","IIEPH","inv_mothcw","inv_mothdg","inv_mother","IILEQ","IILEU","inv_mothsi","inv_mothnc","inv_mothnt","inv_mothot","inv_mothpa","inv_mothsa","IILEX0B","inv_mothte","inv_mothtg","IILEY89","IILEY7P","inv_sawfly","inv_snailf","inv_snailt","ILARA","IZSPN","IICLL,IIPLE","inv_trubug","SenSp") 
+SGCN_SortOrder <- c("Bird","Mammal","Amphibian","Snake","Turtle","Lizard","Frog","Salamander","Fish","Invertebrate - Mussels","Invertebrate - Butterflies","Invertebrate - Moths","Invertebrate - Dragonflies and Damselflies","Invertebrate - Bees","Invertebrate - Cave Invertebrates","Invertebrate - Freshwater Snails","Invertebrate - Terrestrial Snails","Invertebrate - Crayfishes","Invertebrate - Spiders","Invertebrate - Beetles","Invertebrate - True bugs","Invertebrate - Grasshoppers","Invertebrate - Caddisflies","Invertebrate - Craneflies","Invertebrate - Sawflies","Invertebrate - Sponges","Invertebrate - Stoneflies","Invertebrate - Mayflies","Sensitive Species") 
 
 # Latex Formating Variables  ##  not sure if this is needed anymore.
 col <- "\\rowcolor[gray]{.7}" # for table row groups  https://en.wikibooks.org/wiki/LaTeX/Colors
